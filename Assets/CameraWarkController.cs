6828af0488bb5d67476da6664b6c22bb790263d2
TFujiwara1111 <71181922+TFujiwara1111@users.noreply.github.com>
2020-10-22 15:57:27 +0900

    Initial commit

---
+++ Assets/CameraWarkController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraWarkController : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Camera subCamera;
+    public Text StartText = null;
+    GameObject clickedGameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public IEnumerator method()
+    {
+        //サブカメラをオンにする　メインをオフ
+        mainCamera.enabled = false;
+        subCamera.enabled = true;
+
+        //ようこそをゲームシーンに表示する
+        StartText.text = "ようこそ○○じまへ";
+
+        //表示させる
+        StartText.gameObject.SetActive(true);
+
+        //2秒待つ
+        yield return new WaitForSeconds(2);
+
+        //２秒の後は非表示にする
+        StartText.gameObject.SetActive(false);
+
+        //選択をゲームシーンに表示する
+        StartText.text = "スタート位置を選んでください";
+
+        //表示させる
+        StartText.gameObject.SetActive(true);
+
+        //2秒待つ
+        yield return new WaitForSeconds(3);
+
+        //２秒の後は非表示にする
+        StartText.gameObject.SetActive(false);
+
+        //もしボタンをクリックをされたら
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = subCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit))
+            {
+                //本当にキューブタグにたっちされたか判定する
+                clickedGameObject = hit.collider.gameObject;
+                if (hit.collider.gameObject.tag == "CubeTag")
+                {
+                    //車に選んだキューブのところに配置する
+
+                }
+            }
+        }
+    }
+}
---
+++ Assets/MainCameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCameraController : MonoBehaviour
+{
+    //車のオブジェクト
+    private GameObject car;
+    //車とカメラの距離
+    private float difference;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //車のオブジェクトを取得
+        this.car = GameObject.Find("Car");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //車の位置に合わせてカメラの位置を移動  2.0fは車からみたカメラの高さ
+        this.transform.position = this.car.transform.position - car.transform.forward * 2.0f + new Vector3(0.0f, 2.0f, 0.0f); 
+
+        this.transform.rotation = this.car.transform.rotation;
+    }
+}
